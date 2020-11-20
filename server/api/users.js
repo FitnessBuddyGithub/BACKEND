f7e79b14985e9c25a2b02e766c4d78512e6d7941
Yufan Zhang <mikyla.c.zhang@gmail.com>
2020-11-20 15:35:14 -0500

    allow fronend access

--- server/api/users.js
+++ server/api/users.js
@@ -25,6 +25,14 @@ router.get('/', async (req, res, next) => {
       // send everything to anyone who asks!
       attributes: ['id', 'email', 'location']
     })
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Max-Age', '1800')
+    res.setHeader('Access-Control-Allow-Headers', 'content-type')
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'PUT, POST, GET, DELETE, PATCH, OPTIONS'
+    )
     res.json(users)
   } catch (err) {
     next(err)
--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
+var cors = require('cors')
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
@@ -43,6 +44,7 @@ passport.deserializeUser(async (id, done) => {
 const createApp = () => {
   // logging middleware
   app.use(morgan('dev'))
+  app.use(cors())
 
   // body parsing middleware
   app.use(express.json())
