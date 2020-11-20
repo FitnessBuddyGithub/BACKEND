7361ffa492347964f90ea01925bc8a2f6abf30f2
Yufan Zhang <mikyla.c.zhang@gmail.com>
2020-11-20 12:52:16 -0500

    changed SRID constant try 6

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -31,7 +31,7 @@ const User = db.define('user', {
     type: Sequelize.ENUM('male', 'female')
   },
   location: {
-    type: Sequelize.GEOMETRY('POINT', 4326)
+    type: Sequelize.GEOMETRY('POINT')
   }
 })
 
